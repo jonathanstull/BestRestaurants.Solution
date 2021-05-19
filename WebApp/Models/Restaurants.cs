521218c023e2b81d32dedb8a7bd2fe8ebf569ce4
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-19 11:31:01 -0700

    adds Cuisines and Restaurants models
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

---
+++ WebApp/Models/Restaurants.cs
@@ -0,0 +1,12 @@
+
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
