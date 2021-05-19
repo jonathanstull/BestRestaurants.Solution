30d9e62925ba997e2a8f952545afc1a9768eefed
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-19 14:35:36 -0700

    :card_file_box: update schema tables with data for cuisines and other controller logic
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- best_restaurants.sql
+++ best_restaurants.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `best_restaurants` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 USE `best_restaurants`;
--- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: best_restaurants
 -- ------------------------------------------------------
@@ -9,7 +9,7 @@ USE `best_restaurants`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,14 +23,24 @@ USE `best_restaurants`;
 
 DROP TABLE IF EXISTS `cuisines`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `cuisines` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
   `Description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuisines`
+--
+
+LOCK TABLES `cuisines` WRITE;
+/*!40000 ALTER TABLE `cuisines` DISABLE KEYS */;
+INSERT INTO `cuisines` VALUES (1,'Farm to Table','Home grown vegetables and enviromentally-friendly protein prepared at your local farm served directly to your table from our kitchen'),(2,'Sushi','Classic rolls, nigiri, sashimi, and other delectables from Japan.'),(3,'Pacific Northwest','Serving dishes like Salmon, risotto, and local pork with flavors of local fruits and veggies.'),(4,'Asian-Fusion','Mixing flavors from Korea and Mexico; adapted for the American pallette.'),(5,'French','Flavors from the country who brought you the croissant'),(6,'Indian','Cuisine with flavors of safron and naan'),(7,'Vietnamese','Rich soups and noodle-based dishes infused with lemongrass and other traditional Vietnamese flavors');
+/*!40000 ALTER TABLE `cuisines` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `restaurants`
@@ -38,7 +48,7 @@ CREATE TABLE `cuisines` (
 
 DROP TABLE IF EXISTS `restaurants`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `restaurants` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `CuisineId` int(11) NOT NULL,
@@ -48,6 +58,15 @@ CREATE TABLE `restaurants` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+LOCK TABLES `restaurants` WRITE;
+/*!40000 ALTER TABLE `restaurants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurants` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -58,4 +77,4 @@ CREATE TABLE `restaurants` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-19 13:19:55
+-- Dump completed on 2021-05-19 14:33:43
