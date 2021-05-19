9b835157c37f01714837cd1143d4248cd51c988b
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-19 11:18:50 -0700

    update SQL file to create schema
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- best_restaurants.sql
+++ best_restaurants.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `best_restaurants` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `best_restaurants`;
 -- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: best_restaurants
@@ -56,4 +58,4 @@ CREATE TABLE `restaurants` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-19 10:54:37
+-- Dump completed on 2021-05-19 11:16:58
